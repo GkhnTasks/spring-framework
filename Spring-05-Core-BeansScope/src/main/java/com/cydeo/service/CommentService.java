b2905e9582391548d6a37288982c59bf939c3302
gokhan.guzelyurt <Gok26han@>
2022-02-23 03:06:33 +0100

    BeanScope

--- Spring-05-Core-BeansScope/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-05-Core-BeansScope/src/main/java/com/cydeo/service/CommentService.java
@@ -5,10 +5,13 @@ import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-
+//@Scope("prototype")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
 
 
