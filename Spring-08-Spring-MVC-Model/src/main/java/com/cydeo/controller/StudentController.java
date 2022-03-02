fef9ecd0d485604cec25cf5a25c4fd4ef5b76fc5
gokhan.guzelyurt <Gok26han@>
2022-03-02 03:36:27 +0100

    Spring MVC-Model

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping("/welcome")
+    public String homepage(Model model){
+
+        model.addAttribute("name","Cydeo");
+        model.addAttribute("course","MVC");
+
+        return "/student/welcome";
+    }
+
+    @RequestMapping("/gg")
+    public String homepage2(Model model){
+
+        model.addAttribute("name","Cydeo");
+        model.addAttribute("course","MVC");
+
+        return "/student/welcome";
+    }
+}
