fe8b11c62b7a20089463b2daef172c603a7734e9
gokhan.guzelyurt <Gok26han@>
2022-04-20 02:46:31 +0200

    Mapping

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Department.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Department.java
@@ -14,6 +14,9 @@ public class Department extends BaseEntity {
     private String department;
     private String division;
 
+    @OneToOne(mappedBy = "department")
+    private Employee employee;
+
     public Department(String departmentName, String division) {
         this.department = department;
         this.division = division;
