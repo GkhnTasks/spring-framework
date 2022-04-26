10ec78ead812dd7f60b2d9026eb1c1ef1d905823
gokhan.guzelyurt <Gok26han@>
2022-04-27 00:27:06 +0200

    Queries

---
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@Data
+public class Employee extends BaseEntity{
+
+    private String firstName;
+    private String lastname;
+    private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private Integer salary;
+
+    private Department department;
+
+    private Region region;
+
+}
