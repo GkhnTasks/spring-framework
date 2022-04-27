e089136159ff577ef3d958780ded0e61525dc83e
gokhan.guzelyurt <Gok26han@>
2022-04-27 03:04:35 +0200

    Queries

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -2,8 +2,12 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
@@ -19,7 +23,36 @@ public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     //Display all employees where last name starts with ''
     List<Employee> findByLastNameStartingWith(String pattern);
 
+    //Display all employees with salary higher than  ' '
+    List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    //Display all employees with salaries less the ' "
+    List<Employee> findBySalaryLessThanEqual(Integer salary);
+
+    //Display all employees that has been hired between " "  and " "
+    List<Employee> findByHireDateBetween(LocalDate startDate,LocalDate endDate);
+
+    //Display all employees where salaries greater and equal to " " in order
+    List<Employee> findBySalaryGreaterThanOrderBySalaryDesc(Integer salary);
+
+    //Display top unique 3 employees that is making less than " "
+    List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
+
+    //Display all employees that do not have email adress
+//    List<Employee> findByEmailIsNotNull(String  email);
 
 
+
+    @Query("SELECT  e FROM  Employee e WHERE e.email='sdubber7@t-online.de'")
+    Employee getEmployeeDetail();
+
+    @Query("SELECT e.salary FROM Employee  e WHERE  e.email='sdubber7@t-online.de'")
+    Integer getEmployeeSalary();
+
+    @Query("SELECT e FROM  Employee  e WHERE  e.email=?1")
+    Optional<Employee> getEmployeeDeatil(String Email);
+
+    @Query("select e from Employee  e where  e.email=?1 and  e.salary=?2")
+    Employee getEmployeeDetail(String email,int salary);
 
 }
