c584aaf2d34c62e7011a3b9c39cd5018812d2f7f
gokhan.guzelyurt <Gok26han@>
2022-06-05 21:18:53 +0200

    Consume FeignClient

--- Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/client/EmployeeClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/client/EmployeeClient.java
@@ -3,6 +3,7 @@ package com.cydeo.client;
 import com.cydeo.dto.Employee;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(url = "https://dummyapi.io",name = "EMPLOYEE-CLIENT")
 public interface EmployeeClient {
@@ -10,6 +11,7 @@ public interface EmployeeClient {
     //https://dummyapi.io/data/v1/user?limit=10
 
     @GetMapping("/data/v1/user?limit=10")
-    Employee getEmployee();
+    Employee getEmployee(@RequestHeader("app-id") String id);
+
 
 }
